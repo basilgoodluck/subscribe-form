d76d4b1d21325138af10741ae7aaa5fbcc8e2210
Basil Goodluck <basilgoodluck22@gmail.com>
2023-12-16 06:27:00 +0100

    Updated JS

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var nameError = document.getElementById("nameError")
 
 
 
-function validate(){
+function validateName(){
     let name = document.getElementById("fName")
     if (name.value == ""){
         name.classList.add("error");
@@ -16,5 +16,40 @@ function validate(){
     }
     else{
         name.classList.add("correct")
+        return true
+    }
+}
+function validateEmail(){
+    let email = document.getElementById("email")
+    if (email.value == ""){
+        email.classList.add("error");
+        email.classList.remove("correct")
+        return false
+    }
+    else if(!email.value.match(/^[A-Za-z]\._\[0-9]*[A-Za-z]*[\.][a-z]{2,4}$/)){
+        email.classList.add("error");
+        email.classList.remove("correct")
+        return false
+    }
+    else{
+        email.classList.add("correct")
+        return true
+    }
+}
+function validatePhone(){
+    let phone = document.getElementById("phoneNumber")
+    if (phone.value == ""){
+        phone.classList.add("error");
+        phone.classList.remove("correct")
+        return false
+    }
+    else if(!phone.value.match(/^[0-9]{11}$/)){
+        phone.classList.add("error");
+        phone.classList.remove("correct")
+        return false
+    }
+    else{
+        phone.classList.add("correct")
+        return true
     }
 }
